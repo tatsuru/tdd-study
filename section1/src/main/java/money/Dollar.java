cbeefc92bcbfe225c6f2083ec7c20e28c8d7759b
wtatsuru <wtatsuru@hatena.ne.jp>
2019-09-15 19:17:33 +0900

    section1 MoneyTest 通った

--- section1/src/main/java/money/Dollar.java
+++ section1/src/main/java/money/Dollar.java
@@ -1,10 +1,13 @@
 package money;
 
 class Dollar {
-  int amount;
-  Dollar(int amount) {
-  }
+    int amount;
 
-  void times(int multipler) {
-  }
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times(int multipler) {
+        amount *= multipler;
+    }
 }
