667dd392e3845dd62958c825d0b95ff37adcadf8
wtatsuru <wtatsuru@hatena.ne.jp>
2019-10-27 20:41:16 +0900

    Franc もファクトリメソッド化してテストコードからサブクラスが消えた

--- section1/src/main/java/money/Money.java
+++ section1/src/main/java/money/Money.java
@@ -10,4 +10,7 @@ abstract class Money {
     static Money dollar(int amount) {
         return new Dollar(amount);
     }
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
 }
