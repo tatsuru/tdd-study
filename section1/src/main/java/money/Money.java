648916e77cd82e0620b8ba5cda382bea8e0adeee
wtatsuru <wtatsuru@hatena.ne.jp>
2019-10-27 20:58:20 +0900

    コンストラクタも親クラスで持てるようになったぞ

--- section1/src/main/java/money/Money.java
+++ section1/src/main/java/money/Money.java
@@ -3,6 +3,10 @@ package money;
 abstract class Money {
     protected int amount;
     protected String currency;
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
     abstract Money times(int multiplier);
     public String currency() {
         return currency;
